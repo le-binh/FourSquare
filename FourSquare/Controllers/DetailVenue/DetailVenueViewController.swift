42a5129ee1d6805c504133b191712a5b2409553d
hovansu <hovansu.it@gmail.com>
2016-08-04 18:06:06 +0700

    #3: Fix Line In Side Menu #3: Fix Connect With Home And Side Menu When Change Root

---
+++ FourSquare/Controllers/DetailVenue/DetailVenueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailVenueViewController.swift
+//  FourSquare
+//
+//  Created by Mylo Ho on 8/4/16.
+//  Copyright © 2016 Le Van Binh. All rights reserved.
+//
+
+import UIKit
+
+class DetailVenueViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
