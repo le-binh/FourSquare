f1b1a3c77f9b81af625e13615a273f8fc960863b
hovansu <hovansu.it@gmail.com>
2016-08-04 11:57:29 +0700

    #3: Fixed Code Review

--- FourSquare/Controllers/Background/BackgroundViewController.swift
+++ FourSquare/Controllers/Background/BackgroundViewController.swift
@@ -12,27 +12,29 @@ import SwiftUtils
 
 class BackgroundViewController: LGSideMenuController {
 
+    // MARK:- Properties
+
     var leftViewController = LeftSideMenuViewController()
-
     var allMenuItems: [ItemMenu] = []
-
     var activeMenuItems: [ItemMenu] = []
+
+    // MARK: Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.leftViewSwipeGestureEnabled = false
-        for i in 0..<6 {
-            guard let menuItem = MenuItemsSlide(rawValue: i) else {
-                continue
-            }
-            let item = ItemMenu(item: menuItem, active: false)
-            allMenuItems.append(item)
-        }
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.updateItem), name: NotificationCenterKey.updateItemsMenu, object: nil)
+        self.configureDefaultListMenuItem()
+        self.setUpNotificationCenter()
     }
 
-    func loadMenuView(initWithRootViewController: UIViewController, style: LGSideMenuPresentationStyle) {
+    override func leftViewWillLayoutSubviewsWithSize(size: CGSize) {
+        super.leftViewWillLayoutSubviewsWithSize(size)
+        leftViewController.view.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+    }
 
+    // MARK:- Public Funtions
+
+    func loadMenuView(initWithRootViewController: UIViewController, style: LGSideMenuPresentationStyle) {
         let leftSlideViewWidth = kScreenSize.width * (2 / 3)
         super.rootViewController = initWithRootViewController
         leftViewController = LeftSideMenuViewController.vc()
@@ -42,12 +44,6 @@ class BackgroundViewController: LGSideMenuController {
         self.leftViewBackgroundColor = Color.Brown62
         leftViewController.view.backgroundColor = UIColor.clearColor()
         self.leftView().addSubview(leftViewController.view)
-
-    }
-
-    override func leftViewWillLayoutSubviewsWithSize(size: CGSize) {
-        super.leftViewWillLayoutSubviewsWithSize(size)
-        leftViewController.view.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
     }
 
     func updateItem(notification: NSNotification) {
@@ -56,10 +52,25 @@ class BackgroundViewController: LGSideMenuController {
                 self.allMenuItems[item.item.rawValue] = item
             }
         }
-
         self.activeMenuItems = self.allMenuItems.filter({
             $0.active == true
         })
+    }
+
+    // MARK:- Private Functions
+
+    private func setUpNotificationCenter() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.updateItem), name: NotificationCenterKey.updateItemsMenu, object: nil)
+    }
+
+    private func configureDefaultListMenuItem() {
+        for i in 0..<6 {
+            guard let menuItem = MenuItemsSlide(rawValue: i) else {
+                continue
+            }
+            let item = ItemMenu(item: menuItem, active: false)
+            allMenuItems.append(item)
+        }
     }
 
 }
